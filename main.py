25b8cc6513fa15fed5389817a0cad0c592f7ba81
Ahmedkhan428 <ahmedkhaaaan2012@gmail.com>
2026-09-16 23:20:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -195,5 +195,6 @@ def delete_session(session_id: str):
 
 if __name__ == "__main__":
     import uvicorn
+    import os
     port = int(os.environ.get("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
